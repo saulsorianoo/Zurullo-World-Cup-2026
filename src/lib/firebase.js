0f89eeeea7b261a04b9dcd8efe89635029cb3397
Saül <saulsorianot@gmail.com>
2026-06-10 17:27:52 +0200

    fix: Revertir nombre de app firebase innecesario

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
   appId:             import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig, "ZurulloAppV2");
+const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 
